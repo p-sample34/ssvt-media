ab53c275d27bed3e3cf3e349bc14cc51e77eea30
p-sample34 <psample34@gmail.com>
2025-11-03 21:29:06 +0530

    Delete ssvt-media directory

--- ssvt-media/models/media.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const mediaSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  type: { type: String, enum: ["image", "video"], required: true }
-}, { timestamps: true });
-
-export default mongoose.model("Media", mediaSchema);
